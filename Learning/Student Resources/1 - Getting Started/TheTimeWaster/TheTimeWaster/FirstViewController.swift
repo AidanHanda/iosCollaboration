60672d620e6f19735cbfc74565c8a99e19328015
Aidan Handa <handa.aidan@gmail.com>
2017-12-07 14:41:57 -0600

    Worked on making a working version

---
+++ Learning/Student Resources/1 - Getting Started/TheTimeWaster/TheTimeWaster/FirstViewController.swift
@@ -0,0 +1,42 @@
+//
+//  FirstViewController.swift
+//  TheTimeWaster
+//
+//  Created by Aidan Handa on 12/6/17.
+//  Copyright © 2017 Aidan Handa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LabelAF.text = String(appDelegate.getCounter()!);
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+        LabelAF.text = String(appDelegate.getCounter()!);
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func BigBadButton(_ sender: Any) {
+        appDelegate.incrCounter()
+        LabelAF.text = String(appDelegate.getCounter()!)
+        appDelegate.saveData()
+    }
+    
+    @IBOutlet weak var LabelAF: UILabel!
+    
+
+
+}
+
---
+++ Learning/Student Resources/1 - Getting Started/TheTimeWaster/TheTimeWaster/SecondViewController.swift
@@ -0,0 +1,58 @@
+//
+//  SecondViewController.swift
+//  TheTimeWaster
+//
+//  Created by Aidan Handa on 12/6/17.
+//  Copyright © 2017 Aidan Handa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func plus10(_ sender: Any) {
+        if(appDelegate.getCounter()! >= 10)
+        {
+            appDelegate.setIncr(amount: 10)
+            appDelegate.setCounter(value: appDelegate.getCounter()! - 10)
+            
+        }
+    }
+    
+    @IBAction func plus100(_ sender: Any) {
+        if(appDelegate.getCounter()! >= 1000)
+        {
+            appDelegate.setIncr(amount: 100)
+            appDelegate.setCounter(value: appDelegate.getCounter()! - 100)
+        }
+    }
+    
+    @IBAction func plus1000(_ sender: Any) {
+        if(appDelegate.getCounter()! >= 100000)
+        {
+            appDelegate.setIncr(amount: 1000)
+            appDelegate.setCounter(value: appDelegate.getCounter()! - 1000)
+        }
+    }
+
+}
+
